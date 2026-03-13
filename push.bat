03f43edb1b8cad8498c5da682a366d7d0d4efcd3
Dynnova <makesethpos@gmail.com>
2026-03-13 13:41:40 +0700

    Update push.bat

--- push.bat
+++ push.bat
@@ -1,18 +1,18 @@
 @echo off
-echo 📦 Export database ke JSON...
+echo Export database ke JSON...
 node src/export.js
 if errorlevel 1 (
-  echo ❌ Export gagal!
+  echo Export gagal!
   pause
   exit /b 1
 )
 
 echo.
-echo 📤 Push ke GitHub...
+echo Push ke GitHub...
 git add web/public/cosplay.json
 git commit -m "update: cosplay data %date% %time%"
 git push
 
 echo.
-echo ✅ Selesai! Netlify akan auto deploy dalam ~1 menit.
+echo Selesai! Netlify akan auto deploy dalam ~1 menit.
 pause
